a056bda93c298fa34b0527664721d9e5c1fb2ef2
K23 <hashersmasher@proton.me>
2024-02-22 13:48:19 -0300

    validate and satinize password and email

---
+++ api/pkg/tango_validator/validator.go
@@ -0,0 +1,40 @@
+package tango_validator
+
+import (
+	"fmt"
+	"net/mail"
+	"text/template"
+
+	passwordstrenght "github.com/k23dev/tango/pkg/passwordstrenghtchecker"
+	"github.com/k23dev/tango/pkg/tango_errors"
+)
+
+// Validate and satinize email
+func ValidateEmail(email string) (string, error) {
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		return "", tango_errors.ReturnDefault("Email", "Invalid email", 0)
+	}
+	return template.HTMLEscapeString(email), nil
+}
+
+// Validate and satinize password
+func ValidatePassword(password string, minLen, maxLen int) (string, error) {
+
+	passChecker := passwordstrenght.NewPasswordStrenght(minLen, maxLen)
+
+	if password != "" {
+		if !passChecker.HasMinLen(password) {
+			return "", tango_errors.ReturnDefault("Password", fmt.Sprintf("Password must have %d chars minium", minLen), 0)
+		}
+		password = template.HTMLEscapeString(password)
+		return password, nil
+	} else {
+		return "", &tango_errors.DefaultError{
+			Name:    "Password",
+			Code:    0,
+			Message: "Invalid password",
+		}
+	}
+
+}
