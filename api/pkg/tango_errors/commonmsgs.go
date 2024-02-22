70fa18584752eb807d8d225eb7714de6fdf8141d
K23 <hashersmasher@proton.me>
2024-02-22 13:48:00 -0300

    change update tango_errors

--- api/pkg/tango_errors/commonmsgs.go
+++ api/pkg/tango_errors/commonmsgs.go
@@ -3,7 +3,11 @@ package tango_errors
 import "fmt"
 
 func MsgIDNotFound(id int) string {
-	return fmt.Sprintf("No record found with the id %d", id)
+	return fmt.Sprintf("Record not found with the id %d", id)
+}
+
+func MsgNotFound() string {
+	return fmt.Sprintf("Record not found")
 }
 
 func MsgZeroRecordsFound() string {
